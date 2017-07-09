43e1199fa660a297eb367815b2874a2b632dd324
Mohamed Ali Bouhoum <ali.bouhoum@hotmail.com>
2017-07-09 15:14:10 +0100

    The website

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//custom script here
+
+//smooth scroll
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+       
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+$(document).ready(function(){
+
+	$("#download").on('click', function(event){
+		$("#modal").show();
+	});
+	$(".close").on('click', function(event){
+		$("#modal").hide();
+	})
+
+})
